97ef1ac3f78cd03b0f92d5a4aa73b4248b0b61c4
NguyenHuy2003 <2121050061@student.humg.edu.vn>
2024-06-03 15:55:05 +0700

    first commit

---
+++ lib/firebase_auth_screen.dart
@@ -0,0 +1,56 @@
+// ignore: depend_on_referenced_packages
+// ignore_for_file: sized_box_for_whitespace
+
+// ignore: depend_on_referenced_packages
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_home/auth_gate.dart';
+import 'package:smart_home/profile_page.dart';
+
+class FirebaseAuthScreen extends StatefulWidget {
+  const FirebaseAuthScreen({super.key});
+
+  @override
+  State<FirebaseAuthScreen> createState() => _FirebaseAuthScreenState();
+}
+
+class _FirebaseAuthScreenState extends State<FirebaseAuthScreen> {
+  final auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: LayoutBuilder(
+        builder: (context, constraints) {
+          return Row(
+            children: [
+              Visibility(
+                visible: constraints.maxWidth >= 720,
+                child: Expanded(
+                  child: Container(
+                    height: double.infinity,
+                    child: const Placeholder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: constraints.maxWidth >= 720
+                    ? constraints.maxWidth / 2
+                    : constraints.maxWidth,
+                child: StreamBuilder<User?>(
+                  stream: auth.authStateChanges(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return const ProfilePage();
+                    }
+                    return const AuthGate();
+                  },
+                ),
+              )
+            ],
+          );
+        },
+      )),
+    );
+  }
+}
